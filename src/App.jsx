fcb89976df1f9bcf62103eb09822d3865ebb470c
michelelongo97 <m.longo7x@gmail.com>
2025-04-24 12:34:07 +0200

    feat: add milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+  console.log(results);
+
+  useEffect(() => {
+    if (!query.trim()) {
+      setResults([]);
+      return;
+    }
+    fetch(
+      `https://boolean-spec-frontend.vercel.app/freetestapi/products?search=${query}`
+    )
+      .then((res) => res.json())
+      .then((data) => setResults(data))
+      .catch((error) => console.error(error));
+  }, [query]);
 
   return (
     <div>
@@ -12,6 +27,15 @@ function App() {
         onChange={(e) => setQuery(e.target.value)}
         placeholder="Search..."
       />
+      {results.length > 0 && (
+        <div>
+          {results.map((result) => (
+            <p key={result.id}>
+              <h2>{result.name}</h2>
+            </p>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
